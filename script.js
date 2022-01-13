a01dc159aabfbd329e1bc14a6fc60ca74a940b28
jacky32 <patrki@atlas.cz>
2022-01-13 20:12:44 +0100

    added RGB option, enhanced visuals, enter func

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 const gridBox = document.getElementById("grid-box");
-document.getElementById("reset").addEventListener("click", function() {resetGrid()});
+let rgbToggle = false;
+document.getElementById("button-reset").addEventListener("click", () => {resetGrid()});
+document.addEventListener("keyup", (e) => {if(e.key === "Enter") {resetGrid();}});
+document.getElementById("button-RGB").addEventListener("click", () => {if(rgbToggle) rgbToggle = false; else rgbToggle = true;});
+document.getElementById("button-RGB").addEventListener("Touch", () => {if(rgbToggle) rgbToggle = false; else rgbToggle = true;});
+gridBox.addEventListener("mousemove", (e) => {changeColor(e);})
+
 createGrid(16);
 
 
-gridBox.addEventListener("mousemove", function(e) {
-    if(e.target.classList == "grid-item") {
-        e.target.classList.add("changed-grid-item");
+function changeColor(e){
+    if(e.target.classList.contains("grid-item")) {
+        if(rgbToggle){
+            if(e.target.classList.contains("rgb")){
+                let BGC = window.getComputedStyle(e.target).backgroundColor.slice(4,-1).split(", ");
+                for(i=0;i<3;i++) {
+                    if(Number(BGC[i])>25){
+                        BGC[i] = Number(BGC[i]) - 25;
+                    }
+                    else BGC[i] = 0;
+                }
+                e.target.style.backgroundColor = "rgb("+ BGC[0] +","+BGC[1]+","+BGC[2]+")";
+            }
+            else {
+                e.target.style.backgroundColor = "rgb("+Math.random()*256+","+Math.random()*256+","+Math.random()*256+")"
+                e.target.classList.add("rgb");
+            }
+        }
+        else {
+            e.target.style.backgroundColor = "black";
+        }
     }
-})
+}
 
 function createGrid(gridAmount) {
     gridBox.style.setProperty('--grid-columns', gridAmount);
@@ -30,5 +54,9 @@ function resetGrid() {
     if(input.value <=100 && input.value >0) {
         removeGrid();
         createGrid(input.value);
+    }
+    else if (input.value==0) {
+        removeGrid();
+        createGrid(16);
     }
 }
